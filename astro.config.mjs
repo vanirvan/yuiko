989ed2d3c89c795ce8eab55f74acac127882dff9
vanirvan <vanir1812@gmail.com>
2024-09-23 16:21:44 +0700

    add fmc: mortgage-repayment-calculator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 
@@ -8,6 +7,6 @@ export default defineConfig({
   integrations: [tailwind(), mdx()],
   redirects: {
     "/transform": "/",
-    "/fmc": "/",
-  },
+    "/fmc": "/"
+  }
 });
